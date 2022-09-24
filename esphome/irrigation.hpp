17f7cde2a8c0a27d8ac34de0ef434faa174cd192
MiguelJPen <miguelpencar@gmail.com>
2022-09-24 19:26:53 +0200

    Modified irrigation time calculation

--- esphome/irrigation.hpp
+++ esphome/irrigation.hpp
@@ -7,12 +7,15 @@
 
 #include "esphome.h"
 
+#define DAY_SECS 86400
+#define CADUCA "Hoja caduca"
+#define PERENNE "Hoja perenne"
+#define FRUTALES "Frutales"
+
 using namespace std;
 
-void set_new_irrigation_time();
-void set_irrigation(time_t, int, int);
-
-pair<string, string> get_irrigation_time(time_t, time_t, string);
+pair<string, string> get_irrigation_time(time_t, time_t, float, float, string, int, int, int, int);
+void set_time_at(time_t &, int, int, int, int); // Internal
 time_t get_next_irrigation_day(time_t, time_t, float, float, string);
 time_t get_next_time_at(int, int, int, int);
 string add_three_minutes_from_now();
@@ -20,131 +23,62 @@ string manual_set(int, string);
 string delete_first_timestamp(string);
 time_t add_n_days(time_t, int); // Internal
 
-void set_new_irrigation_time() {
-    int time_per_day = 15, no_water_days = 0, freq_day = 1, next_from_today = 0, last_day = 0;
-    float mean_tmp = id(mean_temp);
-    float mean_hum = id(mean_humid);
-    float min_hum = id(min_humid);
-    float precip = id(precip_yesterday);
-    //time_t old_next_irrigation = atoi((id(north_next_irrigation_day).state).c_str());
-    time_t last_irrigation = id(north_last_automatic_execution);
-
-    time_t now_timestamp = id(time_sntp).now().timestamp;
-    //double diff_secs = difftime(old_next_irrigation, now_timestamp);
-    //if (diff_secs > 0) no_water_days = (int) floor(diff_secs / (3600 * 24));
-
-    //ESP_LOGD("set_new_irrigation_time", "DIFF SECS: %f, NO WATER DAYS: %i", diff_secs, no_water_days);
-
-    /*id(north_timestamps_on).publish_state("");
-    id(north_timestamps_off).publish_state("");
-    id(south_timestamps_on).publish_state("");
-    id(south_timestamps_off).publish_state("");*/
-
-
-    // Set how many irrigation days
-    /*if (precip >= 20) {
-        if (mean_tmp <= 20)
-            no_water_days = 4;
-        else
-            no_water_days = max(2, no_water_days);
-    }
-    else if (precip >= 7){
-        if (mean_tmp <= 13)
-            no_water_days = 4;
-        else if (mean_tmp > 13 && mean_tmp <= 20)
-            no_water_days = max(2, no_water_days);
-        else no_water_days = max(1, no_water_days);
-    }*/
-    //id(north_next_irrigation_day).publish_state(to_string(add_n_days(now_timestamp, no_water_days)));
-
-    // Set the time per day
-    time_per_day += (int) (pow(2, 3.2 + max((float)0, (mean_tmp - 10)/5)));
-
-    if (mean_tmp <= 14) {
-        freq_day = 3;
-    }
-    else if (mean_tmp > 14 && mean_tmp < 19) {
-        freq_day = 2;
-    }
-    else if (mean_tmp >= 19) {
-        freq_day = 1;
-    }
-    if (min_hum < 25 || mean_hum < 50)
-        time_per_day += 20;
-    else if (min_hum < 45)
-        time_per_day += 15;
-
-    // Set the timestamps for the irrigation
-    if (no_water_days == 0) { // If it hasn't rained
-        double diff_secs = difftime(now_timestamp, last_irrigation);
-        if (diff_secs > 0) last_day = (int) floor(diff_secs / (3600 * 24));
-        int new_day = max(freq_day - last_day - 1, 0);
-
-        //ESP_LOGD("set_new_irrigation_time", "LAST DAY: %i, NEW DAY: %i, FREQ DAY: %i", last_day, new_day, freq_day);
-
-        set_irrigation(now_timestamp, new_day, time_per_day);
-        set_irrigation(now_timestamp, new_day + freq_day, time_per_day);
-    }
-    else {
-        set_irrigation(now_timestamp, no_water_days, time_per_day);
-        set_irrigation(now_timestamp, no_water_days + freq_day, time_per_day);
-    }
-}
-
-void set_irrigation(time_t today, int days_ahead, int mins) {
-    time_t new_irrigation_day = today + (days_ahead * 3600 * 24);
-    struct tm new_time = *localtime(&new_irrigation_day);
-    string north_on = id(north_timestamps_on).state;
-    string north_off = id(north_timestamps_off).state;
-    string south_on = id(south_timestamps_on).state;
-    string south_off = id(south_timestamps_off).state;
-    string comma_north_on = (north_on.empty() ? "" : ",");
-    string comma_north_off = (north_off.empty() ? "" : ",");
-    string comma_south_on = (south_on.empty() ? "" : ",");
-    string comma_south_off = (south_off.empty() ? "" : ",");
-
-    if (new_time.tm_mon % 2 == 0) new_time.tm_hour = 9;
-    else new_time.tm_hour = 17;
-    new_time.tm_min = 0;
-    new_time.tm_sec = 0;
-
-    time_t aux = mktime(&new_time);
-    north_on.append(comma_north_on + to_string(aux));
-    south_on.append(comma_south_on + to_string(aux + 2 * 60));
-    north_off.append(comma_north_off + to_string(aux + mins * 60));
-    south_off.append(comma_south_off + to_string(aux + (mins + 2) * 60));
-
-    //ESP_LOGD("set_irrigation", "TIME PER DAY: %i", mins);
-
-    north_on = update_list(north_on);
-    north_off = update_list(north_off);
-    south_on = update_list(south_on);
-    south_off = update_list(south_off);
-    id(north_timestamps_on).publish_state(north_on);
-    id(north_timestamps_off).publish_state(north_off);
-    id(south_timestamps_on).publish_state(south_on);
-    id(south_timestamps_off).publish_state(south_off);
-}
-
-pair<string, string> get_irrigation_time(time_t next_irrigation, time_t last_auto_irrigation, float mean_hum, float mean_tmp, string irrigation_mode, 
+pair<string, string> get_irrigation_time(time_t next_irrigation, time_t last_auto_irrigation, float mean_tmp, float mean_hum, float min_hum, string irrigation_mode, 
 int h_morn, int m_morn, int h_afte, int m_afte) {
     time_t today_timestamp = id(time_sntp).now().timestamp;
-    int irrigation_frequency = 1;
-    int days_to_next_irrigation = max(0, (int) floor(difftime(today_timestamp, next_irrigation) / (60 * 60 * 24)));
-    int days_since_last_irrigation = max(0, (int) floor(difftime(today_timestamp, last_auto_irrigation) / (60 * 60 * 24)));
-    string turn_on = "", turn_off = "";
+    int irrigation_frequency = 1, mins = 20;
+    int days_to_next_irrigation = max(0, (int) floor(difftime(today_timestamp, next_irrigation) / DAY_SECS));
+    int days_since_last_irrigation = max(0, (int) floor(difftime(today_timestamp, last_auto_irrigation) / DAY_SECS) - 1);
+    string turn_on = "", turn_off = "", comma = ",";
 
-    if (irrigation_mode.compare("Hoja caduca")) {
-        
+    if (irrigation_mode.compare(CADUCA) == 0 || irrigation_mode.compare(PERENNE) == 0) {
+        mins = max(mins, (int) ((51.932 * pow(mean_tmp, 0.524782)) - 133));
+
+        if (mean_tmp <= 10) irrigation_frequency = 5;
+        else if (mean_tmp <= 13) irrigation_frequency = 4;
+        else if (mean_tmp <= 16) irrigation_frequency = 3;
+        else if (mean_tmp <= 19) irrigation_frequency = 2;
     }
-    else if (irrigation_mode.compare("Hoja perenne")) {
+    else if (irrigation_mode.compare(FRUTALES) == 0) {
+        mins = max(mins, (int) ((1.39268 * pow(mean_tmp, 1.44504)) + 45));
 
+        // Everyday irrigation, except when it rains a lot
     }
-    else if (irrigation_mode.compare("Frutales")) {
 
+    if (min_hum < 25 || mean_hum < 45) mins += 25;
+    else if (min_hum < 45) mins += 15;
+
+    time_t irrigation_timestamp = max(today_timestamp, max(today_timestamp + (days_to_next_irrigation * DAY_SECS), 
+                                        today_timestamp + ((irrigation_frequency - days_since_last_irrigation) * DAY_SECS)));
+    set_time_at(irrigation_timestamp, h_morn, m_morn, h_afte, m_afte);
+    turn_on.append(to_string(irrigation_timestamp));
+    turn_off.append(to_string(irrigation_timestamp + (mins * 60)));
+
+    irrigation_timestamp += irrigation_frequency * DAY_SECS;
+    set_time_at(irrigation_timestamp, h_morn, m_morn, h_afte, m_afte);
+    turn_on.append(comma + to_string(irrigation_timestamp));
+    turn_off.append(comma + to_string(irrigation_timestamp + (mins * 60)));
+
+    turn_on = update_list(turn_on);
+    turn_off = update_list(turn_off);
+
+    return make_pair(turn_on, turn_off);
+}
+
+void set_time_at(time_t &day, int h_morn, int m_morn, int h_afte, int m_afte) {
+    struct tm new_time = *localtime(&day);
+
+    if (new_time.tm_mon % 2 == 0) {
+        new_time.tm_hour = h_morn;
+        new_time.tm_min = m_morn;
     }
+    else {
+        new_time.tm_hour = h_afte;
+        new_time.tm_min = m_afte;
+    }
+    new_time.tm_sec = 0;
 
-    return {"", ""};
+    day = mktime(&new_time);
 }
 
 time_t get_next_irrigation_day(time_t old_next_irrigation, time_t last_auto_irrigation, float precipitation, float mean_tmp, string irrigation_mode) {
@@ -152,7 +86,7 @@ time_t get_next_irrigation_day(time_t old_next_irrigation, time_t last_auto_irri
     adjust the irrigation days and prevent water waste and flooding the roots. */
     time_t today_timestamp = id(time_sntp).now().timestamp;
     struct tm now_time = *localtime(&today_timestamp);
-    int no_water_days = max(0, (int) floor(difftime(old_next_irrigation, today_timestamp) / (60 * 60 * 24)));
+    int no_water_days = max(0, (int) floor(difftime(old_next_irrigation, today_timestamp) / DAY_SECS));
 
     // Set a fixed hour, we want to set the day
     now_time.tm_hour = 12;
@@ -160,7 +94,9 @@ time_t get_next_irrigation_day(time_t old_next_irrigation, time_t last_auto_irri
     now_time.tm_sec = 0;
     today_timestamp = mktime(&now_time);
 
-    if(irrigation_mode.compare("Hoja caduca")) {
+    //ESP_LOGD("get_next_irrigation_day", "Option of my select: %s", irrigation_mode.c_str());
+
+    if(irrigation_mode.compare(CADUCA) == 0) {
         // No irrigation during winter
         if (now_time.tm_mon > 10) {
             now_time.tm_year++;
@@ -187,7 +123,7 @@ time_t get_next_irrigation_day(time_t old_next_irrigation, time_t last_auto_irri
             }
         }
     }
-    else if(irrigation_mode.compare("Hoja perenne")) {
+    else if(irrigation_mode.compare(PERENNE) == 0) {
         if (now_time.tm_mon < 2 || now_time.tm_mon > 10) {
             if (precipitation > 5) no_water_days = max(no_water_days, max(5, int(precipitation / 5)));
         }
@@ -200,10 +136,10 @@ time_t get_next_irrigation_day(time_t old_next_irrigation, time_t last_auto_irri
             }
         }
     }
-    else if(irrigation_mode.compare("Frutales")) {
+    else if(irrigation_mode.compare(FRUTALES) == 0) {
         if (precipitation >= 10) {
             if (mean_tmp < 18) no_water_days = max(no_water_days, max(3, int(((precipitation + 10) / 10))));
-            else if (mean_tmp < 25) no_water_days = max(no_water_days, max(2, int((precipitation / 15))));
+            else if (mean_tmp < 25) no_water_days = max(no_water_days, max(2, int((precipitation / 20))));
             else no_water_days = max(no_water_days, 1);
         }
     }
